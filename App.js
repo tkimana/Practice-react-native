68c85a59c380b6798de74a14a319b3bd67f2a5ff
Theodore Kimana <kimana.theodore@gmail.com>
2019-10-16 17:23:42 -0500

    working

--- App.js
+++ App.js
@@ -7,11 +7,10 @@ class App extends React.Component {
      state={
        recipes: [], 
        displayRecipes: [],
-       title: '',
-       image_url:'',
-       publisher:'',
+      //  title: '',
+      //  image_url:'',
+      //  publisher:'',
       
-     
      }
 
 
@@ -29,16 +28,13 @@ class App extends React.Component {
     });
    
   }
-  onChange=(title, publisher, image_url)=>{
-    this.setState({
-      title: title,
-      publisher: publisher,
-      image_url: image_url
-    })
-    console.log(title, publisher,image_url)
-  }
 
-  
+
+  onChange=(title, publisher, image_url)=>{
+   console.log(title, publisher, image_url)
+    
+  }
+ 
  handleSubmit=(title, image_url, publisher)=>{
   
   fetch('http://localhost:3000/recipes', {
@@ -47,7 +43,7 @@ class App extends React.Component {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      recipe:{
+      recipes:{
         title: title,
         image_url: image_url,
         publisher: publisher
@@ -66,25 +62,25 @@ class App extends React.Component {
  
 
   render(){
-    // console.log(this.state.recipes)
+   
   return (
-
-    <ImageBackground  source={require('./components/backgroundImage/img/My Post.jpg')} >
-    <StatusBar hidden/>
+//  <View style={styles.container}>
+    <ImageBackground   source={require('./components/backgroundImage/img/My Post.jpg')} >
+    {/* <StatusBar hidden/> */}
       
   <TextInput style={styles.InputBox} onChangeText={(title) => this.onChange(title, 'title')}
-        value={this.state.title} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Title" placeholderTextColor="#fffff"/> 
+        value={this.state.title} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Title" /> 
 
      <TextInput style={styles.PublisherInput} onChangeText={(publisher) => this.onChange(publisher, 'publisher')}
-        value={this.state.publisher} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Publisher" placeholderTextColor="#fffff"/> 
+        value={this.state.publisher} underlineColorAndroid='rgba(0,0,0,0)' placeholder="Publisher" /> 
 
     <TextInput style={styles.InputField} onChangeText={(image_url) => this.onChange(image_url, 'image_url')}
-    value={this.state.image_url} underlineColorAndroid='rgba(0,0,0,0)' placeholder="image_url" placeholderTextColor="#fffff"/>
+    value={this.state.image_url} underlineColorAndroid='rgba(0,0,0,0)' placeholder="image_url" />
   <Button  title="Submit" color="#f194ff" onPress={() => this.handleSubmit}
   />
      <PostContainer recipes={this.state.recipes}/>
     </ImageBackground>
-
+    // </View>
   );
   }
 }
@@ -106,7 +102,7 @@ const styles = StyleSheet.create({
    borderRadius: 25,
    paddingHorizontal:16,
    fontSize: 20,
-   color: '#fffff',
+  //  color: '#fffff',
    marginVertical:10,
    padding:4,
    flex: 1,
@@ -120,7 +116,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingHorizontal:16,
     fontSize: 20,
-    color: '#fffff',
+    // color: '#fffff',
     marginVertical:20,
     padding:5,
     flex: 1,
@@ -133,7 +129,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingHorizontal:16,
     fontSize: 20,
-    color: '#fffff',
+    // color: '#fffff',
     marginVertical:20,
     padding:5,
     flex: 1,
