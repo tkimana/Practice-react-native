341a14e7a395e72ac9785fdf3da3f24e97045a0a
Theodore Kimana <kimana.theodore@gmail.com>
2019-11-18 17:45:15 -0600

    d

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, Image, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, Image} from 'react-native';
 
 
 function Separator() {
