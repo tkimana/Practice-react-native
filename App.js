598601f6e3793a02eb12f8a7693a0d2ec127367c
Theodore Kimana <kimana.theodore@gmail.com>
2020-01-08 14:46:08 -0600

    s

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View,StatusBar, Button, ScrollView, Form, FlatList} from 'react-native';
+import { StyleSheet, Text, View,  Button, ScrollView, Form, FlatList} from 'react-native';
 import PostContainer from './components/PostContainer';
 import RecipeForm from './components/RecipeForm';
 
