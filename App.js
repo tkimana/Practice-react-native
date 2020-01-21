a4fbead5b69c89214ac14663503973d497fc369b
Theodore Kimana <kimana.theodore@gmail.com>
2020-01-21 13:16:56 -0600

    d

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,  ScrollView, Form, FlatList} from 'react-native';
+import {View,  ScrollView, FlatList} from 'react-native';
 import PostContainer from './components/PostContainer';
 import RecipeForm from './components/RecipeForm';
 
